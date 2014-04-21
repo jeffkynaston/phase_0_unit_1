64153138c040518d17f0b75430198dce0cb07bf6
Jeff Kynaston <kynaston.jeffrey@gmail.com>
2014-04-20 18:48:27 -0700

    finished gps reflections

--- week_3/8_gps1_2/our_solution.js
+++ week_3/8_gps1_2/our_solution.js
@@ -53,7 +53,7 @@ function Client(name, age, quote, comission) {
 
 //YOUR CODE HERE!
 
-var shooterMcGavin = new Client("Shooter McGavin", 48, "Just stay out of my way... or you'll pay. Listen to what I say.");
+var shooterMcGavin = new Client("Shooter McGavin", 48, "Just stay out of my way... or you'll pay. Listen to what I say.", .10);
 console.log(shooterMcGavin.constructor === Client);
 console.log(shooterMcGavin.age === 48);
 console.log(shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to what I say.");
@@ -72,6 +72,22 @@ shooterMcGavin.display()
 
 // ** BONUS **
 
-// added comission property
+// We didn't get time to do a bunch of bonus work, but we did add a another proprety 
+// to our Client object, comission, to make sure we get our cut. 
+
 
 //  Your Reflection:
+
+// I thought that this was a great challenge. It was my first introduction to Client 
+// Constructors and really helped me to distinguish between them and object literals. 
+// Constructors seem very similar to CLasses in Ruby; making that connection went a 
+// long way to helping me understand the concept. 
+//
+// This was also my first introduction to using the prototype method. I looked into it
+// more and it seems like a powerful method to use in JavaScript. This blog post
+// http://timkadlec.com/2008/01/using-prototypes-in-javascript/ explained it really well. 
+//
+// Working with a partner on this (Austin) was also a great experience. We had similiar
+// levels of experience with JavaScript, which made for a good dialog back and forth when
+// we were figuring out the challenge. We also discovered that we had both worked at the same 
+// company in SF - small world! 
